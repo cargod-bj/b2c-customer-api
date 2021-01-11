ff8c40d85830f1da838cb8197648e4cf0f1f5ad5
Yunaw <291580817@qq.com>
2021-01-11 15:55:14 +0800

    增加source枚举

--- enums/enums.go
+++ enums/enums.go
@@ -1,29 +1,6 @@
 package enums
 
-import "sort"
-
 const SmsTimeOut int = 10 //单位分钟
 const (
-	C2B = 1
+	C2B = "C"
 )
-
-var marketingSourceText = map[int]string{
-	C2B: "C",
-}
-
-func MarketingSourceText(code int) string {
-	return marketingSourceText[code]
-}
-
-func MarketingSourceList() []int {
-	return getKeys(marketingSourceText)
-}
-
-func getKeys(maps map[int]string) []int {
-	keys := make([]int, 0, len(maps))
-	for mapKey, _ := range maps {
-		keys = append(keys, mapKey)
-	}
-	sort.Ints(keys)
-	return keys
-}
